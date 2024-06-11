ef26b6ec481e74c3cb84aea6e185e4bf508f7f00
Belinda Zhu <redpandachocolate@gmail.com>
2024-06-11 15:15:45 -0700

    Created project files

---
+++ rectangledatabase/src/main/java/rectangledatabase/rectangledatabase/RectangledatabaseApplication.java
@@ -0,0 +1,13 @@
+package rectangledatabase.rectangledatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RectangledatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RectangledatabaseApplication.class, args);
+	}
+
+}
---
+++ rectangledatabase/src/test/java/rectangledatabase/rectangledatabase/RectangledatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package rectangledatabase.rectangledatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RectangledatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
